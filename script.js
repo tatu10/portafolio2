4112e4dd7c15b80149e05f3e82decd5faeac3a4d
tatu10 <146473111+tatu10@users.noreply.github.com>
2023-11-30 20:50:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function seleccionar(){
 function efectoHabilidades(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 100){
+    if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
